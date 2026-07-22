ef6525baf5067bb6265169d04d86eaa67a235c9b
Preetham990 <preethamshetty990@gmail.com>
2026-07-22 12:30:31 +0530

    Update movieApi.js

--- Frontend/src/api/movieApi.js
+++ Frontend/src/api/movieApi.js
@@ -1,8 +1,10 @@
 import api from './api';
-export const getAllMovies = () => api.get('/movies/getallmovies');
-export const getMoviesByGenre = (genre) => api.get('/movies/getmovicesbygenre', { params: { genre } });
-export const getMoviesByLanguage = (language) => api.get('/movies/getmovicesbylanguage', { params: { language } });
-export const getMovieByTitle = (title) => api.get('/movies/getmovicesbytitle', { params: { title } });
-export const addMovie = (payload) => api.post('/movies/addmovie', payload);
-export const updateMovie = (id, payload) => api.put(`/movies/updatemovie/${id}`, payload);
-export const deleteMovie = (id) => api.delete(`/movies/deletemovie/${id}`);
+
+export const createShow = (payload) =>
+  api.post('/api/shows/createshows', payload);
+
+export const getAllShows = () =>
+  api.get('/api/shows/getallshows');
+
+export const getShowsByMovie = (movieId) =>
+  api.get(`/api/shows/getshowsbymovie/${movieId}`);
