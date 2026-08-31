c10dd6c8d60ab6eda3872fa6e2700f9d586f1cc0
Preetham990 <preethamshetty990@gmail.com>
2026-08-31 10:08:44 +0530

    Update Securityconfig.java

--- Backend/src/main/java/com/spring/bookMyMovies/Security/Securityconfig.java
+++ Backend/src/main/java/com/spring/bookMyMovies/Security/Securityconfig.java
@@ -122,7 +122,7 @@ public class Securityconfig {
 
         configuration.setAllowedOrigins(List.of(
             "http://localhost:5173",
-            "https://bookmymoviehere.netlify.app"
+            "https://bookmovieshere.netlify.app"
         ));
 
         configuration.setAllowedMethods(List.of(
